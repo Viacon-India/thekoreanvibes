43c5e1de07f33c241802748ad1bd541756a29fa3
Suraj Karmakar <125034428+surajkkar@users.noreply.github.com>
2024-12-30 16:54:49 +0530

    added gradient scroller to other pages

--- wp-content/themes/korean-theme/category.php
+++ wp-content/themes/korean-theme/category.php
@@ -7,24 +7,24 @@ $post_count = $GLOBALS['wp_query']->found_posts;
 $post_per_page = get_option('posts_per_page');
 $cat_id = $archive_object->term_id;
 $parent_id = $archive_object->parent;
-$hex_color_1 = get_term_meta($cat_id, 'hex_code_1', true);
+$primary_color = get_term_meta($cat_id, 'hex_code_1', true);
 if (!empty($parent_id)) {
     $count = get_posts(array('category' => $parent_id, 'numberposts' => -1));
     $page = ceil(count($count) / $post_per_page);
 }
-if (empty($hex_color_1) && !empty($parent_id)) {
-    $hex_color_1 = get_term_meta($parent_id, 'hex_code_1', true);
+if (empty($primary_color) && !empty($parent_id)) {
+    $primary_color = get_term_meta($parent_id, 'hex_code_1', true);
 }
 $child_cat = (empty($parent_id)) ? get_terms('category',  array('child_of' => $cat_id)) : get_terms('category',  array('child_of' => $parent_id)); ?>
 
 <style>
     .category-anchor-active:before {
         content: '';
-        background-color: <?php echo $hex_color_1; ?>;
+        background-color: <?php echo $primary_color; ?>;
     }
     body::-webkit-scrollbar-thumb
     {
-        background-color: <?php echo $hex_color_1; ?>;
+        background-color: <?php echo $primary_color; ?>;
     }
 </style>
 
@@ -43,7 +43,7 @@ $child_cat = (empty($parent_id)) ? get_terms('category',  array('child_of' => $c
                     <li class="bread-list text-[#9E9E9E]"><?php echo strip_tags(single_cat_title()); ?></li>
                 </ul>
             </div>
-            <h1 class="category-title" style="color:<?php echo $hex_color_1; ?>;">
+            <h1 class="category-title" style="color:<?php echo $primary_color; ?>;">
                 <?php echo strip_tags(single_cat_title()); ?>
             </h1>
             
@@ -64,11 +64,11 @@ $child_cat = (empty($parent_id)) ? get_terms('category',  array('child_of' => $c
                     <div class="flex-none">
                         <ul class="flex px-1 gap-4 category-wrapper ">
                             <li class="category-anchor-li">
-                                <button class="category-anchor<?php echo (empty($parent_id)) ? ' category-anchor-active' : ''; ?>" data-hex_color="<?php echo $hex_color_1; ?>" data-cat_id="<?php echo (empty($parent_id)) ? $cat_id : $parent_id; ?>" data-page="<?php echo (!empty($parent_id)) ? $page : $page_count; ?>">ALL</button>
+                                <button class="category-anchor<?php echo (empty($parent_id)) ? ' category-anchor-active' : ''; ?>" data-hex_color="<?php echo $primary_color; ?>" data-cat_id="<?php echo (empty($parent_id)) ? $cat_id : $parent_id; ?>" data-page="<?php echo (!empty($parent_id)) ? $page : $page_count; ?>">ALL</button>
                             </li>
                             <?php foreach (array_values($child_cat) as $child) : ?>
                                 <li class="category-anchor-li">
-                                    <button class="category-anchor<?php echo ($child->term_id == $cat_id) ? ' category-anchor-active' : ''; ?>" data-hex_color="<?php echo $hex_color_1; ?>" data-cat_id="<?php echo $child->term_id; ?>" data-page="<?php echo ceil($child->count / $post_per_page); ?>"><?php echo $child->name; ?></button>
+                                    <button class="category-anchor<?php echo ($child->term_id == $cat_id) ? ' category-anchor-active' : ''; ?>" data-hex_color="<?php echo $primary_color; ?>" data-cat_id="<?php echo $child->term_id; ?>" data-page="<?php echo ceil($child->count / $post_per_page); ?>"><?php echo $child->name; ?></button>
                                 </li>
                             <?php endforeach; ?>
                         </ul>
@@ -77,11 +77,11 @@ $child_cat = (empty($parent_id)) ? get_terms('category',  array('child_of' => $c
                 <?php if (have_posts()) : ?>
                     <div id="load_more_div" class="grid-wrapper">
                         <?php while (have_posts()) : the_post();
-                            get_template_part('template-parts/default', 'card', array('hex_color' => $hex_color_1));
+                            get_template_part('template-parts/default', 'card', array('hex_color' => $primary_color));
                         endwhile; ?>
                     </div>
                     <div class="button-wrapper" <?php echo ($post_count <= $post_per_page) ? 'style="display: none;"' : ''; ?>>
-                        <button class="view-more" style="background-color:<?php echo $hex_color_1; ?>;" data-hex_color="<?php echo $hex_color_1; ?>" data-page="<?php echo $page_count; ?>" data-cat_id="<?php echo $cat_id; ?>" id="load_more" aria-label="More Post">
+                        <button class="view-more" style="background-color:<?php echo $primary_color; ?>;" data-hex_color="<?php echo $primary_color; ?>" data-page="<?php echo $page_count; ?>" data-cat_id="<?php echo $cat_id; ?>" id="load_more" aria-label="More Post">
                             VIEW MORE
                         </button>
                     </div>
@@ -89,7 +89,7 @@ $child_cat = (empty($parent_id)) ? get_terms('category',  array('child_of' => $c
                     <p class="internal-p pt-[30px]">Sorry, but no post available with "<capital class="uppercase"><?php echo strip_tags(single_cat_title()); ?></capital>".</p>
                 <?php endif; ?>
             </div>
-            <?php get_sidebar('', array('hex_color' => $hex_color_1)); ?>
+            <?php get_sidebar('', array('hex_color' => $primary_color)); ?>
         </div>
     </div>
 </section>
